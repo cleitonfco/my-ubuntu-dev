715b432ae8f8980d447c1880418a5d7ce2b715c4
Cleiton Francisco <cleitonfco@gmail.com>
2009-04-16 23:09:13 -0300

    New Install

--- install.rb
+++ install.rb
@@ -1,32 +1,70 @@
 #!/usr/bin/env ruby
 
-# from http://github.com/technomancy/dotfiles
-
-require 'yaml'
-
-if `whoami`.chomp != "root"
-  puts "You must be root."
-  exit 1
+module Development
+  class Install
+    def self.prompt(question, command)
+      print "#{question}? (y/n): "
+      input = gets.chomp
+      while (input !~ /[yn]/) do
+        print "#{question}? Digite (y) para Sim e (n) para Não: "
+        input = gets.chomp
+      end
+      system command if (input == "y")
+    end
+  end
 end
 
-packs = YAML.load(File.read('packs.yml'))
-gems = YAML.load(File.read('gems.yml'))
+puts "Iniciando..."
 
-if !system "sudo apt-get install #{packs.join(' ')} -y"
-  raise "Couldn't install packages"
-end
+puts "Instalando libs basicas (programação)..."
+system "apt-get -y install gcc build-essential zlib1g zlib1g-dev libxml-ruby libxml2 libxml2-dev libxslt1-dev libyaml-ruby libreadline-ruby libfcgi-ruby1.8 libreadline-ruby1.8 libruby1.8 libopenssl-ruby libzlib-ruby libruby"
+system "apt-get -y install locate curl wget libruby-extras libncurses-ruby libcurses-ruby imagemagick libmagick9-dev"
 
-# install rubygems
-if !File.exist?('/usr/bin/gem')
-  system "wget http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz"
-  system "tar zxf rubygems-1.3.1.tgz"
-  system "cd rubygems-1.3.1/"
-  system "sudo ruby setup.rb"
-  system "sudo gem update --system"
-end
+puts "Instalando libs especificas do Ruby..."
+system "apt-get -y install ruby ruby1.8 irb ruby1.8-dev ruby1.8 ri1.8 rdoc1.8 irb1.8"
 
-system "sudo gem sources -a http://gems.github.com"
-system "sudo gem install rails"
-system "sudo gem install rake" # sometimes we get a false start
-system "sudo gem install #{gems.join(' ')}"
+puts "Instalando bancos de dados sqlite3, MySql e Postgres"
+system "apt-get -y install libdbm-ruby libdbi-ruby sqlite3 sqlite3-ruby libsqlite3-dev libdbd-sqlite3-ruby libsqlite3-ruby mysql-client-5.0 mysql-server-5.0 mysql-admin python-mysqldb libmysql-ruby libmysqlclient15-dev mysql-query-browser libpq-dev libpgsql-ruby pgadmin3 postgresql-8.3 postgresql-client-8.3 postgresql-client-common postgresql-common postgresql-contrib-8.3"
+
+puts "Instalando RubyGems 1.3.2 a partir do Codigo-fonte..."
+system "wget http://rubyforge.org/frs/download.php/55066/rubygems-1.3.2.tgz && tar -xzv rubygems-1.3.2.tgz"
+system "cd rubygems-1.3.2 && ruby setup.rb install"
+system "cd .. && rm -rf rubygems-1.3.2"
+system "gem sources -a http://gems.github.com"
+system "gem update"
+
+puts "Instalando algumas gems..."
+system "gem install rails mongrel rake rack rcov capistrano capistrano-ext hpricot treetop ruby-debug passenger mysql postgres ZenTest authlogic github json rmagick json_pure launchy redgreen syntax mislav-will_paginate jchupp-is_paranoid thoughtbot-paperclip thoughtbot-shoulda faker notahat-machinist rspec rspec-rails cucumber webrat sqlite3-ruby vlad wirble twitter xmpp4r"
+
+puts "Instalando alguns programas básicos..."
+system "apt-get -y install pidgin-libnotify zsnes tree meld rcov msttcorefonts"
+
+Development::Install.prompt "Instalar Git", "apt-get -y install git-core git-doc git-gui gitk"
+Development::Install.prompt "Instalar Filezilla", "apt-get -y install filezilla"
+
+@plugins_gedit = "apt-get -y install gedit-plugins python-webkitgtk python-pyinotify && 
+git clone git://github.com/lexrupy/gmate.git && 
+cd gmate && sh intall.sh && cd .. && rm -rf gmate && 
+git clone git://github.com/oliver/gedit-file-search.git && 
+cp gedit-file-search/file-search.* ~/.gnome2/gedit/plugins/ && rm -rf gedit-file-search && 
+git clone git://github.com/caironoleto/rspec_mate.git && 
+cp rspec_mate/rspec_mate ~/.gnome2/gedit/plugins/ && cp rspec_mate/rspec_mate.gedit-plugin ~/.gnome2/gedit/plugins/ && rm -rf rspec_mate"
+Development::Install.prompt "Instalar plugins do Gedit", @plugins_gedit
+
+@mod_rails = "echo 'deb http://apt.brightbox.net hardy main' > /etc/apt/sources.list.d/brightbox.list && 
+wget -q -O - http://apt.brightbox.net/release.asc | apt-key add - && 
+apt-get update && 
+apt-get -y install libapache2-mod-passenger"
+Development::Install.prompt "Instalar mod_rails", @mod_rails
+
+Development::Install.prompt "Instalar PHP5", "apt-get -y install php5 libapache2-mod-php5 php5-mysql phpmyadmin libapache2-mod-auth-mysql && /etc/init.d/apache2 restart"
+
+puts "Finalizando instalacao..."
+print "Versão do Ruby: "
+system "ruby -v"
+print "Versão do RubyGems: " 
+system "gem -v"
+print "Versão do Rails: " 
+system "rails -v"
+puts "Finalizado!"
 
